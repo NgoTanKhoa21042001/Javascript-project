c4dc7db7516410b6e0212892844d8f5d04219c93
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-06-26 18:57:26 +0700

    first commit

---
+++ Filterable Image Gallery/app.js
@@ -0,0 +1,26 @@
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+
+const filterableCards = document.querySelectorAll(".filterable_cards .card");
+
+console.log(filterButtons, filterableCards);
+
+const filterCards = (e) => {
+  document.querySelector(".active").classList.remove("active");
+
+  e.target.classList.add("active");
+
+  filterableCards.forEach((card) => {
+    // add hide class to hide the card initially
+    card.classList.add("hidden");
+
+    if (
+      card.dataset.name === e.target.dataset.name ||
+      e.target.dataset.name === "all"
+    ) {
+      card.classList.remove("hidden");
+    }
+  });
+};
+filterButtons.forEach((button) =>
+  button.addEventListener("click", filterCards)
+);
