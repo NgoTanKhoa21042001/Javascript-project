73ae6d627f66d559433bb84f6eea678c93b87dfd
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-07-20 16:34:38 +0700

    Responsive Image Gallery with Search Box

--- Filterable Image Gallery/app.js
+++ Filterable Image Gallery/app.js
@@ -2,7 +2,28 @@ const filterButtons = document.querySelectorAll(".filter_buttons button");
 
 const filterableCards = document.querySelectorAll(".filterable_cards .card");
 
-console.log(filterButtons, filterableCards);
+// console.log(filterButtons, filterableCards);
+
+// const filterCards = (e) => {
+//   document.querySelector(".active").classList.remove("active");
+
+//   e.target.classList.add("active");
+
+//   filterableCards.forEach((card) => {
+//     // add hide class to hide the card initially
+//     card.classList.add("hidden");
+
+//     if (
+//       card.dataset.name === e.target.dataset.name ||
+//       e.target.dataset.name === "all"
+//     ) {
+//       card.classList.remove("hidden");
+//     }
+//   });
+// };
+// filterButtons.forEach((button) =>
+//   button.addEventListener("click", filterCards)
+// );
 
 const filterCards = (e) => {
   document.querySelector(".active").classList.remove("active");
@@ -10,9 +31,7 @@ const filterCards = (e) => {
   e.target.classList.add("active");
 
   filterableCards.forEach((card) => {
-    // add hide class to hide the card initially
     card.classList.add("hidden");
-
     if (
       card.dataset.name === e.target.dataset.name ||
       e.target.dataset.name === "all"
@@ -21,6 +40,7 @@ const filterCards = (e) => {
     }
   });
 };
+
 filterButtons.forEach((button) =>
   button.addEventListener("click", filterCards)
 );
